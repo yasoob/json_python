838b1f1e6a3f7766b1f03f6c918aa02760d97ece
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2015-12-27 23:09:03 +0500

    modified the code

--- app.py
+++ app.py
@@ -4,16 +4,24 @@ import requests
 import sys
 
 if len(sys.argv) < 2:
-    print('[Help] Kindly pass in the user_id')
+    print('[Help] python app.py <user_id>\n<user_id> should be from between 0 to 100')
     sys.exit()
+try:
+	user_id = int(sys.argv[-1])
+except ValueError as e:
+	print("[Error] Please input an integer from 0 to 100")
+	sys.exit(-1)
 
-if int(sys.argv[-1]) < 100:
+if user_id < 100:
     user_id = sys.argv[-1]
 else:
     print('[Help] The user_id needs to be less than 101')
     sys.exit()
 
 url = "http://jsonplaceholder.typicode.com/posts/{}".format(user_id)
-data = requests.get(url).json()
+try:
+	data = requests.get(url).json()
+except requests.exceptions.ConnectionError as e:
+	print(e)
 print('[Title]:  '+data['title'])
 print('[Body]:   '+data['body'])
