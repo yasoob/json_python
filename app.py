1844ae581ce0b082e58b1725dcaf52afe177c3ab
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2015-12-28 21:25:25 +0500

    pep8

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ if len(sys.argv) < 2:
     print('[Help] python app.py <user_id>\n<user_id> should be from between 0 to 100')
     sys.exit()
 try:
-	user_id = int(sys.argv[-1])
+    user_id = int(sys.argv[-1])
 except ValueError as e:
-	print("[Error] Please input an integer from 0 to 100")
-	sys.exit(-1)
+    print("[Error] Please input an integer from 0 to 100")
+    sys.exit(-1)
 
 if user_id <= 100 and user_id > 0:
     user_id = sys.argv[-1]
@@ -20,11 +20,11 @@ else:
 
 url = "http://jsonplaceholder.typicode.com/posts/{}".format(user_id)
 try:
-	data = requests.get(url).json()
+    data = requests.get(url).json()
 except requests.exceptions.ConnectionError as e:
-	print("[Error] There was a network error")
-	print(e)
-	sys.exit()
+    print("[Error] There was a network error")
+    print(e)
+    sys.exit()
 
 print('[Title]:  '+data['title'])
 print('[Body]:   '+data['body'])
